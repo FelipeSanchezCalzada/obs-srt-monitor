1d0df9aa8b533290dcddf9d71327c33014308785
Felipe Sánchez <felipesanchezcalzada@gmail.com>
2023-03-29 00:08:53 +0200

    Start Angular Project Install PrimeNG Create the main layout with navbar (top menu)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainLayoutComponent} from 'src/app/layout/main-layout/main-layout.component';
+import {HomePageComponent} from 'src/app/pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainLayoutComponent, children: [
+      { path: 'home', component: HomePageComponent}
+    ]},
+  { path: '**', redirectTo: '/home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ButtonModule} from 'primeng/button';
+import {RippleModule} from 'primeng/ripple';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import {MenubarModule} from 'primeng/menubar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    RippleModule,
+    MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
